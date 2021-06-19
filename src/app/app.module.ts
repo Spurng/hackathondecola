2848dc3e80ac7a22d5f777ca8686ece9086a95f3
Lucas Matos <lucaspmatos@outlook.com>
2021-06-19 14:30:02 -0300

    mudando module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { DepoimentosPageComponent } from './components/depoimentos-page/depoimentos-page.component';
+import { AjudaPageComponent } from './components/ajuda-page/ajuda-page.component';
 import { MenuTemplateComponent } from './components/menu-template/menu-template.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { DepoimentoCardComponent } from './components/depoimento-card/depoimento-card.component';
+import { LeisPaginaComponent } from './components/leis-pagina/leis-pagina.component';
+
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { HomePageComponent } from './components/home-page/home-page.component';
-import { DepoimentosPageComponent } from './components/depoimentos-page/depoimentos-page.component';
-import { DepoimentoCardComponent } from './components/depoimento-card/depoimento-card.component';
-import { LeisPaginaComponent } from './components/leis-pagina/leis-pagina.component';
-import { AjudaPageComponent } from './components/ajuda-page/ajuda-page.component';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 
