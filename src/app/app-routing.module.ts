2d6904a1fdf443f7370b0624c7b5ea170d889a0d
Priscila Pereira de Miranda <priscila.fraser@gmail.com>
2021-06-19 12:16:15 -0400

    menuiniciado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import { DepoimentosPageComponent } from './components/depoimentos-page/depoimentos-page.component';
+import { MenuTemplateComponent } from './components/menu-template/menu-template.component';
+import { LeisPaginaComponent } from './components/leis-pagina/leis-pagina.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: MenuTemplateComponent,
+    children: [
+      {
+        path: '',
+        component: HomePageComponent
+      },
+      {
+        path: 'depoimentos',
+        component: DepoimentosPageComponent
+      },
+      {
+        path: 'leis',
+        component: LeisPaginaComponent
+      }
+    ]
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
