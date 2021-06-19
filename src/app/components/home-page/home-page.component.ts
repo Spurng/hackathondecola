bae880a0aab20748debef97c2ff3fa935cf13d3a
Igor Leonardo Silva <igor-k2010@hotmail.com>
2021-06-19 12:05:48 -0400

    api instituicoes funcionando, remover alteracoes na appmodule e home page antes de fazer o push final

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { InstituicoesMulheresApiModel } from 'src/app/services/instituicoes-mulheres-api-model';
+import { InstituicoesMulheresApiService } from 'src/app/services/instituicoes-mulheres-api.service';
+import { LeisMulheresApiModel } from 'src/app/services/leis-mulheres-api-model';
+import { LeisMulheresApiService } from 'src/app/services/leis-mulheres-api.service';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  instituicoes: InstituicoesMulheresApiModel[] = [];
+  leis: LeisMulheresApiModel[] = [];
 
+​
+  constructor(
+    public InstituicoesMulheres: InstituicoesMulheresApiService,
+    public LeisMulheres: LeisMulheresApiService) { }
+​
   ngOnInit(): void {
+    this.InstituicoesMulheres.get().subscribe({
+      next: (retornoDaApi) => {
+        this.instituicoes = retornoDaApi;
+      }
+    });
+
+    this.LeisMulheres.get().subscribe({
+      next: (retornoDaApi) => {
+        this.leis = retornoDaApi;
+        console.log(this.instituicoes[0]?.descricao)
+      }
+    });
+​
   }
+
+
 
 }
